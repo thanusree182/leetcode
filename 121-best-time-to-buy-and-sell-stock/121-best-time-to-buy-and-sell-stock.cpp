038d69c17064d678a1e1684190e91dc6823e42a4
thanusree182 <86425534+thanusree182@users.noreply.github.com>
2022-04-12 09:09:59 +0530

    Time: 195 ms (31.11%), Space: 93.5 MB (13.32%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& arr) {
+
+        int max_val=0,min_val=INT_MAX;
+        for(int i=0;i<arr.size();i++){
+             min_val=min(min_val,arr[i]);
+            max_val=max(max_val,arr[i]-min_val);
+        }
+         return max_val;   
+    }
+};
+
