36488686841d83808da16772e85d07d2043175d7
thanusree182 <86425534+thanusree182@users.noreply.github.com>
2022-05-26 22:39:30 +0530

    Time: 3 ms (87.15%), Space: 12.2 MB (37.88%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,7 +1,24 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        int n=nums.size(),k,l;
+        for(k=n-2;k>=0;k--){
+            if(nums[k]<nums[k+1]){ //to check breakpoint
+                break;
+            }
+        }
+        if(k<0){
+            reverse(nums.begin(),nums.end()); //if no breakpoint exists we just reverse it
+        }
+        else{  //else we do our method
+            for(l=n-1;l>k;l--){
+                if(nums[l]>nums[k]){
+                    break;   //to find value of l
+                }
+            }
+            swap(nums[k],nums[l]);
+            reverse(nums.begin()+k+1,nums.end());
+        }
         
-        next_permutation(nums.begin(),nums.end());
     }
 };
