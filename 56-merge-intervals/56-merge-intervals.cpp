981099c08cbd90d43405d9f37974af4c3829aff7
thanusree182 <86425534+thanusree182@users.noreply.github.com>
2022-05-30 17:09:14 +0530

    Time: 64 ms (29.91%), Space: 20.1 MB (27.62%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,24 +1,23 @@
-
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
-        if(intervals.size()==0)
-            return intervals;
-        
+        if(intervals.size()==0){
+            return ans;
+        }
         sort(intervals.begin(),intervals.end());
-        vector<int> help=intervals[0];
-        for(auto it: intervals){
-            if(it[0]<=help[1]){  //1st pair gets automatically merged as we took same in help at start and then we update help the max value
-                help[1]=max(it[1],help[1]);
+        vector<int> temp=intervals[0];
+        for(auto it:intervals){
+            if(it[0]<=temp[1]){
+                temp[1]=max(it[1],temp[1]);
             }
             else
             {
-                ans.push_back(help); //if they are not merging we add it to our ans array as it is unique
-                help = it; //updating help 
+                ans.push_back(temp);
+                temp=it;
             }
         }
-        ans.push_back(help);
+        ans.push_back(temp);
         return ans;
     }
 };
