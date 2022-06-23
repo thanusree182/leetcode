5c7b5e073f9844947508d37432a7232ac81ddb2b
thanusree182 <86425534+thanusree182@users.noreply.github.com>
2022-06-23 22:32:13 +0530

    Time: 77 ms (93.45%), Space: 33.7 MB (72.85%) - LeetHub

---
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -0,0 +1,36 @@
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         if(size(nums)==0) return 0;
+//         if(size(nums)==1) return 1;
+//         sort(nums.begin(),nums.end());
+//             int n=nums.size();
+//         int clong=1,longe=0;
+//         for(int i=0;i<n;i++){
+//             if(nums[i+1]==nums[i]){
+//               continue;
+//             }
+//             else if(nums[i+1]==nums[i]+1){
+//                clong++;
+//             }
+//             else{
+//                 longe=max(clong,longe);
+//                 clong=1;
+//             }
+//         }
+//         return max(longe,clong) ;
+//     }
+// };
+ class Solution {
+ public:
+int longestConsecutive(vector<int>& nums) {
+	if(!size(nums)) return 0;
+	sort(begin(nums), end(nums));
+	int longest = 0, cur_longest = 1;
+	for(int i = 1; i < size(nums); i++) 
+		if(nums[i] == nums[i - 1]) continue;
+		else if(nums[i] == nums[i - 1] + 1) cur_longest++; // consecutive element - update current streak length
+		else longest = max(longest, cur_longest), cur_longest = 1;  // reset current streak length
+	return max(longest, cur_longest);
+}
+ };
