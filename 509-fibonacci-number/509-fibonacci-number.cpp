3609efb018301f2b355555e35521212085c3cda2
Gaduputi Thanusree <86425534+thanusree182@users.noreply.github.com>
2022-07-29 11:31:38 +0530

    Time: 4 ms (49.68%), Space: 5.9 MB (39.74%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int f[31]={0};
+    int fib(int n) {
+       if(n<=1){
+           return n;
+       }
+        if(f[n]){
+            return f[n];
+        }
+        return f[n]=fib(n-1)+fib(n-2);
+    }
+};
