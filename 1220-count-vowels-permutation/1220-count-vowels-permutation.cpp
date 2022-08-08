1936ac1361d99958eb2dfb25ebc3f38b9d803b6b
Gaduputi Thanusree <86425534+thanusree182@users.noreply.github.com>
2022-08-08 08:48:05 +0530

    Time: 4 ms (91.28%), Space: 5.9 MB (90.50%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int countVowelPermutation(int n) {
-         long a = 1, e = 1, i = 1, o = 1, u = 1, mod = pow(10, 9)+7;
-        long a2, e2, i2, o2, u2; 
-        
-        for (int j = 2; j <= n; j++) {
-            a2 = (e + i + u) % mod;
-            e2 = (a + i) % mod;
-            i2 = (e + o) % mod;
-            o2 = i;
-            u2 = (o + i) % mod;
-            
-            a = a2, e = e2, i = i2, o = o2, u = u2;
+        long a=1,e=1,i=1,o=1,u=1,mod=pow(10,9)+7;
+        long a1=1,e1=1,i1=1,o1=1,u1=1;
+        for(int j=2;j<=n;j++){
+            a1=(e+i+u)%mod;
+            e1=(a+i)%mod;
+            i1=(e+o)%mod;
+            o1=i;
+            u1=(i+o)%mod;
+            a=a1,e=e1,i=i1,o=o1,u=u1;
         }
-        return (a + e + i + o + u) % mod;
+        return (a+e+i+o+u)%mod;
     }
 };
