127e29c18533a6727d93f037312a6aadddbcf094
Gaduputi Thanusree <86425534+thanusree182@users.noreply.github.com>
2022-08-17 18:20:31 +0530

    Time: 11 ms (15.54%), Space: 8.7 MB (16.15%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
-        vector<string> codes={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        set<string> help;
+        vector<string> codes={
+            ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."
+        };
+          set<string> help;
         for(auto word:words){
+      
             string c="";
-            for(auto ch:word){
-                c+=codes[ch-97];
-            }
-            help.insert(c);
+        for(auto ch:word){
+            
+            c+=codes[ch-97];
+            
+        }
+        help.insert(c);
         }
         return help.size();
     }
+    
 };
