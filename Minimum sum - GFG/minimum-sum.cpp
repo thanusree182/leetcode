3234caa8d722f361ae9b4de2b400720aa64b3bf4
Gaduputi Thanusree <86425534+thanusree182@users.noreply.github.com>
2023-04-11 15:43:43 +0530

    Added solution - LeetHub

--- Minimum sum - GFG/minimum-sum.cpp
+++ Minimum sum - GFG/minimum-sum.cpp
@@ -12,48 +12,35 @@ public:
     // string string_sum(string s1,string s2){
         
     // }
-     string string_sum(string str1,string str2){
-        if (str1.length() > str2.length())
-        swap(str1, str2);
- 
-    // Take an empty string for storing result
-    string str = "";
- 
-    // Calculate length of both string
-    int n1 = str1.length(), n2 = str2.length();
-    int diff = n2 - n1;
- 
-    // Initially take carry zero
-    int carry = 0;
- 
-    // Traverse from end of both strings
-    for (int i=n1-1; i>=0; i--)
-    {
-        // Do school mathematics, compute sum of
-        // current digits and carry
-        int sum = ((str1[i]-'0') +
-                   (str2[i+diff]-'0') +
-                   carry);
-        str.push_back(sum%10 + '0');
-        carry = sum/10;
-    }
- 
-    // Add remaining digits of str2[]
-    for (int i=n2-n1-1; i>=0; i--)
-    {
-        int sum = ((str2[i]-'0')+carry);
-        str.push_back(sum%10 + '0');
-        carry = sum/10;
-    }
- 
-    // Add remaining carry
-    if (carry)
+     string string_sum(string X,string Y){
+         
+        if (X.length() >Y.length())
+        swap(X, Y);
+        int n=X.length(),m=Y.length();
+        int diff=m-n;
+        string str;
+        int carry=0;
+        for(int i=n-1;i>=0;i--){
+            int sum=(X[i]-'0')+(Y[i+diff]-'0')+carry;
+            str.push_back(sum%10+'0');
+            carry=sum/10;
+        }
+        for(int i=m-n-1;i>=0;i--){
+            int sum=(Y[i]-'0')+carry;
+            str.push_back(sum%10+'0');
+            carry=sum/10;
+        }
+         if (carry)
         str.push_back(carry+'0');
- 
-    // reverse resultant string
-    reverse(str.begin(), str.end());
- 
-    return str;
+        
+         
+         int k = str.size()-1;//for removing all zeros which comes at front
+        while(k>=1 && str[k]=='0' ){
+            str.pop_back();
+            k--; 
+        }
+         reverse(str.begin(),str.end());
+          return str;
     }
     string solve(int arr[], int n) {
         // code here
