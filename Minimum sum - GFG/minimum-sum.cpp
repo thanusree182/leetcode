01ab05ae8c4532032bcd579f85bab9512533cd55
Gaduputi Thanusree <86425534+thanusree182@users.noreply.github.com>
2023-04-11 14:56:10 +0530

    Added solution - LeetHub

---
+++ Minimum sum - GFG/minimum-sum.cpp
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    // string string_sum(string s1,string s2){
+        
+    // }
+     string string_sum(string str1,string str2){
+        if (str1.length() > str2.length())
+        swap(str1, str2);
+ 
+    // Take an empty string for storing result
+    string str = "";
+ 
+    // Calculate length of both string
+    int n1 = str1.length(), n2 = str2.length();
+    int diff = n2 - n1;
+ 
+    // Initially take carry zero
+    int carry = 0;
+ 
+    // Traverse from end of both strings
+    for (int i=n1-1; i>=0; i--)
+    {
+        // Do school mathematics, compute sum of
+        // current digits and carry
+        int sum = ((str1[i]-'0') +
+                   (str2[i+diff]-'0') +
+                   carry);
+        str.push_back(sum%10 + '0');
+        carry = sum/10;
+    }
+ 
+    // Add remaining digits of str2[]
+    for (int i=n2-n1-1; i>=0; i--)
+    {
+        int sum = ((str2[i]-'0')+carry);
+        str.push_back(sum%10 + '0');
+        carry = sum/10;
+    }
+ 
+    // Add remaining carry
+    if (carry)
+        str.push_back(carry+'0');
+ 
+    // reverse resultant string
+    reverse(str.begin(), str.end());
+ 
+    return str;
+    }
+    string solve(int arr[], int n) {
+        // code here
+        sort(arr,arr+n);
+        string s1,s2;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                if(arr[i]==0 && s1.length()==0){
+                    continue;
+                }
+                s1+=to_string(arr[i]);
+            }
+            else{
+                if(arr[i]==0 && s2.length()==0){
+                    continue;
+                }
+                s2+=to_string(arr[i]);
+            }
+        }
+        return string_sum(s1,s2);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.solve(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
