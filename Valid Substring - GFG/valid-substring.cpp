fb69153a0743d04093c16fbbcd0537ac7161d1aa
Gaduputi Thanusree <86425534+thanusree182@users.noreply.github.com>
2023-06-05 22:48:56 +0530

    Added solution - LeetHub

---
+++ Valid Substring - GFG/valid-substring.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for c++
+class Solution {
+  public:
+    int findMaxLen(string s) {
+        // code here
+        stack<int> st;
+        int n=s.length();
+        int ans=0;
+        st.push(-1);
+        for(int i=0;i<n;i++){
+            if(s[i]=='(')
+            st.push(i);
+            else{
+                st.pop();
+                if(st.empty())
+                st.push(i); //antey length taggutundi invalid madyalo vachheisndi
+                ans=max(ans,i-st.top());
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S; 
+        Solution ob;
+        cout << ob.findMaxLen(S) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
