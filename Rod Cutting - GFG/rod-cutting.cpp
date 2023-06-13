dd6df228d42a3bd58ef7d0824126c936dc882a13
Gaduputi Thanusree <86425534+thanusree182@users.noreply.github.com>
2023-06-13 12:02:36 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -10,24 +10,24 @@ using namespace std;
 
 class Solution{
   public:
-    int solve(int i,int w,int price[],vector<vector<int>>& dp){
-        if(i==0){
-            return price[0]*w;
-        }
-        if(dp[i][w]!=-1){
-            return dp[i][w];
-        }
-        int not_take=solve(i-1,w,price,dp);
-        int take=INT_MIN;
-        if(w>=i+1){ //a i+1 denotes length of rod at index i
-            take=price[i]+solve(i,w-(i+1),price,dp);
-        }
-        return dp[i][w]=max(take,not_take);
-    }
+   
     int cutRod(int price[], int n) {
         //code here
-        vector<vector<int>> dp (n,vector<int> (n+1,-1));
-        return solve(n-1,n,price,dp);
+        vector<vector<int>> dp (n,vector<int> (n+1,0));
+        for(int i=0;i<=n;i++){
+            dp[0][i]=price[0]*(i);
+        }
+        for(int i=1;i<n;i++){
+            for(int w=0;w<=n;w++){
+                int not_take=dp[i-1][w];
+              int take=INT_MIN;
+                  if(w>=i+1){ //a i+1 denotes length of rod at index i
+                      take=price[i]+dp[i][w-(i+1)];
+                          }
+                          dp[i][w]=max(take,not_take);
+            }
+        }
+        return dp[n-1][n];
     }
 };
 
