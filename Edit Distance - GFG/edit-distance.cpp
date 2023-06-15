a7cd2e409342384b79581ae8cbc2419e0e309bd8
Gaduputi Thanusree <86425534+thanusree182@users.noreply.github.com>
2023-06-15 13:41:38 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.cpp
+++ Edit Distance - GFG/edit-distance.cpp
@@ -5,34 +5,33 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int solve(int i,int j, string &s,string &t,vector<vector<int>>&dp){
-        if(i<0){
-            return j+1;  //if s is ended we need (j+1) character to be added to it to make equal to t
-        }
-        if(j<0){
-            return i+1; //if t is exhausted we need to remove (i+1) length of s to make it equal to t
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s[i]==t[j]){
-            return dp[i][j]=solve(i-1,j-1,s,t,dp);
-        }
-        else{
-            int insert=1+solve(i,j-1,s,t,dp);
-            int delet=1+solve(i-1,j,s,t,dp);
-            int replace=1+solve(i-1,j-1,s,t,dp);
-            
-            return dp[i][j]=min(insert,min(delet,replace));
-        }
-        
-    }
+    
     int editDistance(string s, string t) {
         // Code here
         int n=s.length();
         int m=t.length();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,s,t,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                  if(s[i-1]==t[j-1]){  //1 based indexing
+            dp[i][j]=dp[i-1][j-1];
+        }
+        else{
+            int insert=1+dp[i][j-1];
+            int delet=1+dp[i-1][j];
+            int replace=1+dp[i-1][j-1];
+            
+             dp[i][j]=min(insert,min(delet,replace));
+        }
+            }
+        }
+        return dp[n][m];
     }
 };
 
