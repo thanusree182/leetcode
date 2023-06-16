813dbd58d70eb13cfeb42e3a847343a5dd6e0aea
Gaduputi Thanusree <86425534+thanusree182@users.noreply.github.com>
2023-06-16 10:29:40 +0530

    Added solution - LeetHub

--- Buy Stock 2 - GFG/buy-stock-2.cpp
+++ Buy Stock 2 - GFG/buy-stock-2.cpp
@@ -8,25 +8,27 @@ class Solution{
     
     long long maximumProfit(vector<long long>&prices, int n) {
         // Code here
-        vector<vector<long long>> dp(n+1,vector<long long>(2,0));
-        dp[n][0]=0,dp[n][1]=0; //base case
+         vector<long long> prev(2,0),curr(2,0);
+         
+        prev[0]=0,prev[1]=0; //base case
         //now iteration sholud be written backward like n->1
         for(int i=n-1;i>=0;i--){
             for(int j=0;j<=1;j++){
                 
                  if(j==0){
-            int buy=-prices[i]+dp[i+1][1];
-            int not_buy=dp[i+1][0];
-                dp[i][j]=max(buy,not_buy);
+            int buy=-prices[i]+prev[1];
+            int not_buy=prev[0];
+                curr[j]=max(buy,not_buy);
                          }
                else if(j==1){
-            int sell=prices[i]+dp[i+1][0];
-            int not_sell=dp[i+1][1];
-               dp[i][j]=max(sell,not_sell);
+            int sell=prices[i]+prev[0];
+            int not_sell=prev[1];
+               curr[j]=max(sell,not_sell);
                        }
             }
+            prev=curr;
         }
-        return dp[0][0];
+        return curr[0];
     }
 };
 
