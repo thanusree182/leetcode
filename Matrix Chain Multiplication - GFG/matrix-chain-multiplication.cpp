4c1eb6d46491b69c7872e45bdedb1bb639ffa96d
Gaduputi Thanusree <86425534+thanusree182@users.noreply.github.com>
2023-06-21 09:08:07 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -9,28 +9,30 @@ using namespace std;
 
 class Solution{
 public:
-    int solve(int i,int j,int arr[],vector<vector<int>>& dp){
-        if(i==j){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int mini=1e9;
-        for(int k=i;k<j;k++){
-            int steps=(arr[i-1]*arr[k]*arr[j])+solve(i,k,arr,dp)+solve(k+1,j,arr,dp);
-             mini=min(mini,steps);
-        }
-       
-        return dp[i][j]=mini;
-    }
+   
     int matrixMultiplication(int N, int arr[])
     {
         // code here
-        vector<vector<int>> dp(N,vector<int>(N,-1));
-        return solve(1,N-1,arr,dp);
+        vector<vector<int>> dp(N,vector<int>(N,0));
+       
+        for(int i=0;i<N;i++){
+            dp[i][i]=0; //basecase
+        }
+        for(int i=N-1;i>=1;i--){
+            for(int j=i+1;j<N;j++){
+                int mini=1e9;
+        for(int k=i;k<j;k++){
+            int steps=(arr[i-1]*arr[k]*arr[j])+dp[i][k]+dp[k+1][j];
+             mini=min(mini,steps);
+        }
+        dp[i][j]=mini;
+            }
+            
+        }
+        return dp[1][N-1];
     }
 };
+// TC=O(N^3)
 
 //{ Driver Code Starts.
 
