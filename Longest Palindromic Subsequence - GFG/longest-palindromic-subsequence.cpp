1eafaf964b51cd8dd67a7377bf0d253b11061da4
Gaduputi Thanusree <86425534+thanusree182@users.noreply.github.com>
2023-07-19 15:38:32 +0530

    Added solution - LeetHub

--- Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
+++ Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
@@ -8,38 +8,28 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
-class Solution{   //if we take Longest common subsequence of given string and reverse of that string as palindrome common one is the answer
-  public: 
-  //we need to print it
-    int lcs(int x, int y, string s1, string s2)
-    {
-        // your code here
-        vector<vector<int>> dp  (x+1,vector<int>(y+1,0));
-        for(int i=0;i<=x;i++){
-            dp[i][0]=0;
+class Solution{
+  public:
+    int solve(string& s1,string& s2,int i,int j,int n,vector<vector<int>>&dp){
+        if(i<0 || j<0)
+        return 0;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        if(s1[i]==s2[j]){
+            return dp[i][j]=1+solve(s1,s2,i-1,j-1,n,dp);
         }
-        for(int j=0;j<=y;j++){
-            dp[0][j]=0;
+        else{
+            return dp[i][j]=max(solve(s1,s2,i-1,j,n,dp),solve(s1,s2,i,j-1,n,dp));
         }
-        for(int i=1;i<=x;i++){
-            for(int j=1;j<=y;j++){
-                if(s1[i-1]==s2[j-1]){
-               dp[i][j]=1+dp[i-1][j-1];
-               }
-                else{
-               dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-               }
-            }
-        }
-        return dp[x][y];
     }
     int longestPalinSubseq(string A) {
         //code here
-        string B=A;
-        reverse(A.begin(),A.end());
         int n=A.length();
-        int len=lcs(n,n,A,B);
-        return len;
+        string s1=A;
+        reverse(A.begin(),A.end());
+        string s2=A;
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return solve(s1,s2,n-1,n-1,n,dp);
     }
 };
 
