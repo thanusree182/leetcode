296d79efc300c8a684abbd9151352f7eceb4acf2
Gaduputi Thanusree <86425534+thanusree182@users.noreply.github.com>
2023-08-08 15:09:13 +0530

    Added solution - LeetHub

---
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+	int solvel(int arr[], int n, int x){
+        int low=0;
+        int high=n-1;
+        int last=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==x){
+                last=mid;
+                low=mid+1;
+            }
+            else if(arr[mid]>x){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return last;
+    }
+    int solvef(int arr[], int n, int x){
+        int low=0;
+        int high=n-1;
+        int first=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==x){
+                first=mid;
+                high=mid-1;
+            }
+            else if(arr[mid]>x){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return first;
+    }
+	int count(int arr[], int n, int x) {
+	    // code here
+	    int first=solvef(arr,n,x);
+	    int last=solvel(arr,n,x);
+	    
+	    if(first!=-1 && last!=-1){
+	        return (last-first+1);
+	    }
+	    else{
+	        return 0;
+	    }
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.count(arr, n, x);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
