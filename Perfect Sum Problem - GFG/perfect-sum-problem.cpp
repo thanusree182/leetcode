00434e7b8d3f7813694ac5c3ab234108f6dcba15
Gaduputi Thanusree <86425534+thanusree182@users.noreply.github.com>
2023-09-14 21:54:31 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -7,34 +7,31 @@ class Solution{
 
 	public:
 	int mod=1e9+7;
-	int solve(int arr[],int i,int sum,vector<vector<int>>& dp){
-	   // if(sum==0)
-	   // return 1; 
-	   //upper base case is eliminated as there can be 0 elemenets which add into sub array
-	    if(i==0){
-	        if(sum==0 && arr[0]==0){
+	int solve(int arr[],int i,int target,vector<vector<int>>& dp){
+	     if(i==0){
+	        if(target==0 && arr[0]==0){
 	            return 2;
 	        }
 	   
-	        if(arr[0]==sum || sum==0){
+	        if(arr[0]==target || target==0){
 	                return 1;
 	            }
 	       else  return 0;
 	    }
-	    if(dp[i][sum]!=-1){
-	        return dp[i][sum];
-	    }
-	    int not_take=solve(arr,i-1,sum,dp);
+	    if(dp[i][target]!=-1)
+	    return dp[i][target];
 	    int take=0;
-	    if(sum>=arr[i])
-	    take=solve(arr,i-1,sum-arr[i],dp);
+	    if(target>=arr[i]){
+	        take=solve(arr,i-1,target-arr[i],dp);
+	    }
+	    int not_take=solve(arr,i-1,target,dp);
 	    
-	    return dp[i][sum]= (take%mod+not_take%mod)%mod;
+	    return dp[i][target]=(take%mod+not_take%mod)%mod;
 	}
 	int perfectSum(int arr[], int n, int sum)
 	{
         // Your code goes here
-        vector<vector<int>> dp(n+1,vector<int>(sum+1,-1));
+        vector<vector<int>> dp(n,vector<int>(sum+1,-1));
         return solve(arr,n-1,sum,dp);
 	}
 	  
